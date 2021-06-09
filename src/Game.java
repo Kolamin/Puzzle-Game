06fcf5d28784bf17b28da3f45adf3218fec57d1a
Kolamin <gofranich@gmail.com>
2021-06-09 17:53:41 +0600

    Puzzle game. Part 3.

--- src/Game.java
+++ src/Game.java
@@ -1,22 +1,27 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Game extends JFrame{
     private ArrayList<Point> solution;
+    private ArrayList<PuzzleButton> buttons;
+
     private JPanel panel;
     private BufferedImage source;
-    private BufferedImage resize;
+    private BufferedImage resized;
     private int width, height;
     private final int DESIRED_WIDTH = 400;
 
-    public Game(){
+    public Game() throws IOException {
         initUI();
     }
 
-    public void initUI(){
+    public void initUI() throws IOException {
         solution = new ArrayList<>();
         solution.add(new Point(0, 0));
         solution.add(new Point(0, 1));
@@ -27,6 +32,37 @@ public class Game extends JFrame{
         solution.add(new Point(2, 0));
         solution.add(new Point(2, 1));
         solution.add(new Point(2, 2));
+
+        buttons = new ArrayList<>();
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createLineBorder(Color.gray));
+        panel.setLayout(new GridLayout(4, 3));
+
+        source = loadImage();
+        int h = getNewHeight(source.getWidth(), source.getHeight());
+        resized = resizeImage(source, DESIRED_WIDTH, h, BufferedImage.TYPE_INT_ARGB);
+    }
+
+    private BufferedImage resizeImage(BufferedImage originImage,
+                                      int w, int h, int type){
+        BufferedImage resizedImage = new BufferedImage(width, height, type);
+
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originImage, 0, 0, width, height, null);
+        g.dispose();
+        return resizedImage;
+    }
+
+    private BufferedImage loadImage() throws IOException {
+        BufferedImage bimg = ImageIO.read(new File("flower.jpg"));
+        return bimg;
+    }
+
+    private int getNewHeight(int w, int h){
+        double ration = DESIRED_WIDTH / (double)w;
+        int newHeight = (int)(h * ration);
+        return newHeight;
     }
 
 }
